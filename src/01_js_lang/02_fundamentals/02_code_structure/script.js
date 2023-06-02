2113346668eb88fa5be6cffc5b5479c6c3349924
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-06-02 10:31:18 +0300

    Done with 02_code structure

---
+++ src/01_js_lang/02_fundamentals/02_code_structure/script.js
@@ -0,0 +1,16 @@
+import alert from "alert";
+
+// alert("Hello");
+// alert("World");
+
+// This comment appears on a line of its own
+alert("Hello");
+
+[1, 2, 3].forEach(alert); // This comment follows the statement
+
+/*
+ * An example with two messages.
+ * This is a multiline comment
+ */
+alert("Hello");
+alert("World");
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/02_fundamentals/01_hello_world/script.js";
+import "./01_js_lang/02_fundamentals/02_code_structure/script.js";
