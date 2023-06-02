1149125dd7c9f17976f4aceea4ac0c3a1b3b789d
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-06-02 12:54:18 +0300

    Done with 05_data_types

---
+++ src/01_js_lang/02_fundamentals/05_data_types/script.js
@@ -0,0 +1,62 @@
+import alert from "alert";
+
+// let message = "Hello World";
+// message = 123456; // dynamically typed
+
+// alert(message);
+
+// let n = 12;
+// n = 12.33;
+
+// alert(n);
+
+// alert(1 / 0);
+// alert(Infinity);
+
+// alert("not a number" / 2);
+// alert(NaN + 1);
+// alert(3 * NaN);
+// alert("not a number" / 2 - 1);
+// alert(2 ** 3);
+
+// alert(NaN ** 0);
+
+// console.log(9007199254740991 + 1);
+// console.log(9007199254740991 + 2);
+// const bigNumber = 90071992547409912222n;
+// alert(bigNumber);
+
+// let str = "Hello";
+// let str2 = "Single quotes are ok too";
+// let phrase = `can embed another ${str2}`;
+// alert(phrase);
+
+// let name = "John";
+
+// alert(`Hello, ${name}`);
+// alert(`the result is ${2 + 2}`);
+
+// alert("The result is ${2 + 2}");
+
+// let nameFieldChecked = true;
+// let ageFieldChecked = false;
+
+// let isGreater = 4 > 1;
+
+// alert(isGreater);
+
+// let age = null;
+// console.log(age);
+
+// let age;
+// console.log(age);
+// alert(typeof undefined === "undefined");
+// alert(typeof undefined);
+// alert(typeof 0);
+// alert(typeof 10n);
+// alert(typeof true);
+// alert(typeof "foo");
+// alert(typeof Symbol("id"));
+// alert(typeof Math);
+// alert(typeof null);
+alert(typeof alert);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/02_fundamentals/04_variables/script.js";
+import "./01_js_lang/02_fundamentals/05_data_types/script.js";
