96d93ef155b9b82c7350736edd64c4918e4f923e
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-06-03 22:25:34 +0300

    Done with 07_type_conversion

---
+++ src/01_js_lang/02_fundamentals/07_type_conversions/script.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import alert from "alert";
+
+let value = true;
+// alert(typeof value);
+
+value = String(value);
+// alert(typeof value);
+// alert("6" / "2");
+
+let str = "123";
+// alert(typeof str);
+
+let num = Number(str);
+// alert(typeof num);
+
+let age = Number("Just a string");
+// alert(age);
+
+// alert(Number("   123   "));
+// alert(Number("123z"));
+// alert(Number(true));
+// alert(Number(false));
+
+alert(false / true);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/02_fundamentals/06_alert_promt_confirm/script.js";
+import "./01_js_lang/02_fundamentals/07_type_conversions/script.js";
