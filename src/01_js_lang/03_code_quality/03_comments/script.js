1c02bc7565509fa93a7db8c0046b781306a8be94
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-06-12 12:31:04 +0300

    Done with 03_comments

---
+++ src/01_js_lang/03_code_quality/03_comments/script.js
@@ -0,0 +1,10 @@
+/**
+ * Calculates the result of a number raised to a power.
+ *
+ * @param {number} x - the base number
+ * @param {number} n - the exponent
+ * @return {number} the result of x raised to the power of n
+ */
+function pow(x, n) {
+    return x ** n;
+}
