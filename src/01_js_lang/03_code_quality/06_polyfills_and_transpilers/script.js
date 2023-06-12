edd31454280cd542e593b6cfbb9e6d12a3e92eb9
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-06-12 16:56:43 +0300

    Done with 06_polyfills_and_transpilers

---
+++ src/01_js_lang/03_code_quality/06_polyfills_and_transpilers/script.js
@@ -0,0 +1,1 @@
+console.log(Math.trunc(10.3));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/03_code_quality/02_coding_style/script.js";
+import "./01_js_lang/03_code_quality/06_polyfills_and_transpilers/script.js";
