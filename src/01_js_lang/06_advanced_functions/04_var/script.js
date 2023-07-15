f76b9dec43740e2f8641545837415ec558140416
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-07-15 06:45:37 +0300

    Done with 04_var

---
+++ src/01_js_lang/06_advanced_functions/04_var/script.js
@@ -0,0 +1,60 @@
+// var message = "Hey";
+// console.log(message);
+
+// if (true) {
+//     var message = "Hey";
+// }
+
+// console.log(message);
+
+// for (var i = 0; i < 10; i++) {
+//     var one = 1;
+// }
+
+// console.log(i);
+// console.log(one);
+
+// function sayHi() {
+//     var s = 1;
+//     if (true) {
+//         var d = 2;
+//     }
+
+//     console.log(s);
+// }
+
+// sayHi();
+// console.log(s);
+
+// var user = "John";
+// var user = "Pete";
+
+// console.log(user);
+
+// function sayHi() {
+//     user = "John";
+//     console.log(user);
+
+//     if (false) {
+//         var user;
+//     }
+// }
+
+// sayHi();
+
+// function sayHi() {
+//     console.log(user);
+//     var user = "John";
+// }
+
+// sayHi();
+
+// (function () {
+//     var user = "John";
+//     console.log(user);
+// })();
+
++(function () {
+    var user = "John";
+    console.log(user);
+})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/06_advanced_functions/03_closure/script.js";
+import "./01_js_lang/06_advanced_functions/04_var/script.js";
