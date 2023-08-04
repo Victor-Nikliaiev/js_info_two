3016b0837b565787bfc0f8032f31463cfd68e35c
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-08-04 13:57:16 +0300

    Done with 03_promise_chaining

--- src/01_js_lang/11_async/03_promise_chaining/script.js
+++ src/01_js_lang/11_async/03_promise_chaining/script.js
@@ -120,6 +120,61 @@
 //         console.log(result);
 //     });
 
-fetch("https://jsonplaceholder.typicode.com/users/1")
-    .then(res => res.json())
-    .then(user => console.log(user.name));
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//     .then(res => res.json())
+//     .then(user => console.log(user.name));
+
+const USER_URL =
+    "https://raw.githubusercontent.com/Victor-Nikliaiev/js_info_two/main/src/01_js_lang/11_async/03_promise_chaining/user.json";
+
+function loadJSON(url) {
+    return fetch(url).then(res => res.json());
+}
+
+function loadGithubUser(name) {
+    return loadJSON(`https://api.github.com/users/${name}`);
+}
+
+function showAvatar(githubUser) {
+    return new Promise(function (resolve, reject) {
+        const img = document.createElement("img");
+        img.src = githubUser.avatar_url;
+        document.body.append(img);
+
+        setTimeout(() => {
+            img.remove();
+            resolve(githubUser);
+        }, 3000);
+    });
+}
+
+// fetch(USER_URL)
+//     .then(response => response.json())
+//     .then(user => fetch(`https://api.github.com/users/${user.name}`))
+//     .then(response => response.json())
+//     .then(user => {
+//         return new Promise(resolve => {
+//             const img = document.createElement("img");
+//             img.src = user.avatar_url;
+//             document.body.append(img);
+
+//             setTimeout(() => {
+//                 img.remove();
+//                 resolve(user);
+//             }, 3000);
+//         });
+//     })
+//     .then(user => {
+//         setTimeout(() => {
+//             alert(`Stopped showing ${user.login}`);
+//         }, 1);
+//     });
+
+loadJSON(USER_URL)
+    .then(user => loadGithubUser(user.name))
+    .then(user => showAvatar(user))
+    .then(user => {
+        setTimeout(() => {
+            alert(`Stopped showing ${user.login}`);
+        }, 1);
+    });
