e9d24cd7927b2bb5aab4d10c581fc4d35be2b6da
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-08-31 06:49:28 +0300

    Done with unicode

---
+++ src/01_js_lang/14_miscellaneous/06_unicode/script.js
@@ -0,0 +1,6 @@
+// console.log("\x7A");
+// console.log("\xA9");
+// console.log("\u044F");
+// console.log("S\u0307\u0323");
+
+// console.log("S\u0307\u0323".normalize() === "S\u0323\u0307".normalize());
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/14_miscellaneous/05_big_int/script.js";
+import "./01_js_lang/14_miscellaneous/06_unicode/script.js";
