c84e647f7c61f5e8a3a860a96e12208d5b7d2888
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-08-31 06:38:48 +0300

    Done with bigInt

---
+++ src/01_js_lang/14_miscellaneous/05_big_int/script.js
@@ -0,0 +1,18 @@
+// const bigInt = 90071992547409912222n;
+
+// const sameBigInt = BigInt("90071992547409912222");
+
+// console.log(bigInt == sameBigInt);
+// console.log(bigInt === sameBigInt);
+
+// console.log(1n + 2n);
+// console.log(5n / 2n);
+// // console.log(1n + 1);
+
+// let bigInt = 1n;
+// let num = 2;
+
+// console.log(Number(bigInt) + num);
+
+console.log(1 == 1n);
+console.log(1 === 1n);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-import "./01_js_lang/14_miscellaneous/04_reference_type/script.js";
+import "./01_js_lang/14_miscellaneous/05_big_int/script.js";
