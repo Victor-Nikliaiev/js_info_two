3feea812ff4af9cb20bf61a4f29d0e0bc11d0ca7
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-09-01 09:51:57 +0300

    Done with browser environment introduction

---
+++ src/02_ui/01_document/01_browser_environment/script.js
@@ -0,0 +1,17 @@
+// function sayHi() {
+//     console.log("Hi");
+// }
+
+// // window.sayHi();
+// console.log(window.innerHeight);
+// document.body.style.background = "red";
+
+// setTimeout(() => {
+//     document.body.style.background = "";
+// }, 2000);
+
+console.log(location.href);
+
+if (confirm("Go to Google Search?")) {
+    location.href = "https://google.com";
+}
